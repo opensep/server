5756a1a32f65c190b27f8f8f704dcf64a860ab4f
VIREAK KEOSOKVIBOL <vireakvibol@gmail.com>
2022-07-16 23:07:43 +0700

    remove supertest and config codefactor

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,8 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
-import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 describe('AppController (e2e)', () => {
   let app: NestFastifyApplication;
@@ -12,20 +13,18 @@ describe('AppController (e2e)', () => {
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication<NestFastifyApplication>(new FastifyAdapter());
+    app = moduleFixture.createNestApplication<NestFastifyApplication>(
+      new FastifyAdapter(),
+    );
     await app.init();
   });
 
   it('/ (GET)', async () => {
-    const res = await app.inject({
+    await app.inject({
       method: 'get',
-      url: '/'
-    })
-    expect(200)
-    expect('Hello World!')
-    // return await request(app.getHttpServer())
-    //   .get('/')
-    //   .expect(200)
-    //   .expect('Hello World!');
+      url: '/',
+    });
+    expect(200);
+    expect('Hello World!');
   });
 });
