f1f3ab3390e83ec496b6b0e12b2afa8897f66ed7
VIREAK KEOSOKVIBOL <94353620+vireakvibol@users.noreply.github.com>
2022-10-13 16:11:52 +0700

    remove unnecessary files (#2)

--- src/installer/installer.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { InstallerController } from './installer.controller';
-
-@Module({
-  controllers: [InstallerController]
-})
-export class InstallerModule {}
